c0f5344d46401de79c19c50c710e572f015aacaf
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-04-23 01:34:09 +0900

    [setting] 초기 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ textAlign: "center" }}>
+        <img
+          src="https://i.pinimg.com/564x/99/8a/d7/998ad76c8af67cffb3f6e7f239e791ab.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
