dfdc1c202f19a454ee1d18cd7fadaa62c2d2033c
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-04-25 16:03:25 +0900

    index.css 수정 및 물품상세페이지 CSS 초안 완료

--- src/components/goods/GoodsContents.tsx
+++ src/components/goods/GoodsContents.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import GoodsDescription from "./GoodsDescription";
+import GoodsDetail from "./GoodsDetail";
 
 const GoodsContents: React.FC = () => {
-  return <div></div>;
+  return (
+    <GoodsContentsWrap>
+      <GoodsDescription />
+      <GoodsDetail />
+    </GoodsContentsWrap>
+  );
 };
 
 export default GoodsContents;
+
+const GoodsContentsWrap = styled.div`
+  margin-top: 40px;
+  width: 1010px;
+`;
