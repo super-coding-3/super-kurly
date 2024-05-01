e7732285f717ffc13fda740d3c2263e86d03dd31
ines012 <celeste07102@gmail.com>
2024-05-01 15:59:18 +0800

    [fix] 마이페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import ProductSellList from "./components/ProductsSellList/ProductSellList";
 import Order from "./page/Order";
 import Cart from "./page/Cart";
 import SellerOrder from "./components/sellerOrder/SellerOrder";
+import Mypage from "./page/Mypage";
+
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -21,7 +23,8 @@ const App: React.FC = () => {
         <Route path="/productSellList" element={<ProductSellList />} />
         <Route path="/order" element={<Order />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/sellerOrder" element={<SellerOrder/>} />
+        <Route path="/sellerOrder" element={<SellerOrder />} />
+        <Route path="/mypage" element={<Mypage />} />
         {/* 각자 완성한 페이지 path 및 element 삽입 */}
       </Routes>
     </div>
